7f22f3b7aefb9ea1086405c869efe9eb6c41bc25
Stergios Katsogiannis <kster735@gmail.com>
2026-03-07 22:41:03 +0200

    Added validation annotations on User model's properties.

--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace UsersManagement.Models;
 
 class User
 {
     public Guid Id { get; set; }
+
+    [Required]
     public string? FirstName { get; set; }
+    [Required]
     public string? LastName { get; set; }
-    required public string Email { get; set; }
-    required public string Password { get; set; }
+    [Required]
+    public string? Email { get; set; }
+    [Required]
+    [MinLength(6)]
+    public string? Password { get; set; }
 }
